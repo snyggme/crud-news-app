83a1102e30b3305d615cb395c815be8649f885cd
snyggme <rcbabahin@gmail.com>
2018-11-21 21:18:06 +0300

    google sign in

--- src/App.js
+++ src/App.js
@@ -7,17 +7,35 @@ import Main from './components/Main';
 import Footer from './components/Footer';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        	<Navbar />
-            <Switch>
-                <Route exact path='/' component={Main} />             
-                <Route path='/news' component={News} />
-            </Switch>
-            <Footer />
-      </div>
-    );
+    // componentDidMount() {
+    //     const _onInit = auth2 => {
+    //         console.log('init OK', auth2)
+    //     }
+    //     const _onError = err => {
+    //         console.log('error', err)
+    //     }
+    //     window.gapi.load('auth2', function() {
+    //         window.gapi.auth2
+    //             .init({ 
+    //                 client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+    //             })
+    //             .then(_onInit, _onError)
+    //     })
+    // }
+
+    render() {
+        return (
+          <div>
+          <button onClick={this.signIn}>Log in</button>
+          <button onClick={this.signOut}>Log out</button>
+            	<Navbar />
+                <Switch>
+                    <Route exact path='/' component={Main} />             
+                    <Route path='/news' component={News} />
+                </Switch>
+                <Footer />
+          </div>
+        );
   }
 }
 
